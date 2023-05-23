777b5830bcf93fccb5e17b0c976113db5acf1d5e
LikeMagic85 <like-magic@yandex.ru>
2023-05-23 18:52:11 +0300

    Post Card is implemented

--- app/src/main/java/com/like_magic/vknewsclient/ui/PostCard.kt
+++ app/src/main/java/com/like_magic/vknewsclient/ui/PostCard.kt
@@ -1,1 +1,155 @@
 package com.like_magic.vknewsclient.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.like_magic.vknewsclient.R
+import com.like_magic.vknewsclient.ui.theme.VkNewsClientTheme
+
+@Composable
+fun PostCard() {
+    Card(
+        shape = RoundedCornerShape(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        border = BorderStroke(0.01.dp, MaterialTheme.colorScheme.onBackground)
+
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.template_text)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                painter = painterResource(
+                    id = R.drawable.post_content_image
+                ), contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Statistics()
+        }
+    }
+
+}
+
+@Composable
+private fun PostHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(shape = CircleShape),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Group logo"
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = "dev/null",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "menu",
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+
+    }
+}
+
+@Composable
+private fun Statistics() {
+    Row() {
+        Row(modifier = Modifier.weight(1f)) {
+            IconWithText(iconResId = R.drawable.ic_view_count, text = "500")
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "500")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "200")
+            IconWithText(iconResId = R.drawable.ic_like, text = "400")
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewLight() {
+    VkNewsClientTheme(darkTheme = false) {
+        PostCard()
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDark() {
+    VkNewsClientTheme(darkTheme = true) {
+        PostCard()
+    }
+}
